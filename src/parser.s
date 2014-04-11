b61a27402a467ec466af23e89ba3fd336e1e86e4
Devin Schwab <digidevin@gmail.com>
2014-04-11 18:05:03 -0400

    Parser is now able to tell which command was typed in. Still need to parse the arguments.

--- src/parser.s
+++ src/parser.s
@@ -7,8 +7,8 @@
 # $a1 heap allocated list of arguments
 
 # command codes are as follows
-# play = 1
-# play with args = 2
+# play song = 1
+# play note = 2
 # add rest = 3
 # add note = 4
 # cat = 5
@@ -18,6 +18,7 @@
 # unknown = -1
 parse_input:
 	lb $t0, 0($a0) # get the first character
+	add $t2, $a0, $zero
 
 	li $t1, 0x61 # see if first char is an 'a'
 	beq $t0, $t1, parse_add
@@ -35,8 +36,6 @@ parse_input:
 	j parse_unknown
 
 parse_add:
-	add $t2, $a0, $zero
-
 	# load second character
 	addi $t2, $t2, 1
 	lb $t0, 0($t2)
@@ -52,23 +51,203 @@ parse_add:
 	# see if third character is a d
 	bne $t0, $t1, parse_unknown
 
+	# load the fourth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# see if fourth character is a space
+	li $t1, 0x20
+	bne $t0, $t1, parse_unknown
+
 	addi $a0, $zero, 3
 	add $a1, $zero, $zero
 	jr $ra
 
 parse_play:
+	# load second character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if second character is an l
+	li $t1, 0x6c
+	bne $t0, $t1, parse_unknown
+
+	# load third character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if second character is an a
+	li $t1, 0x61
+	bne $t0, $t1, parse_unknown
+
+	# load fourth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fourth character is a y
+	li $t1, 0x79
+	bne $t0, $t1, parse_unknown
+
+	# load fifth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fifth character is a newline
+	li $t1, 0xA
+	bne $t0, $t1, parse_play_song
+
+	# parse play not command
+	addi $a0, $zero, 2
+	add $a1, $zero, $zero
+	jr $ra
+	
+parse_play_song:	
+	# check if fifth character is a space
+	li $t1, 0x20
+	bne $t0, $t1, parse_unknown
+
+	addi $a0, $zero, 1
+	add $a1, $zero, $zero
 	jr $ra
 
 parse_cat:
+	# load second character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if second character is an a
+	li $t1, 0x61
+	bne $t0, $t1, parse_unknown
+
+	# load third character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if third character is a t
+	li $t1, 0x74
+	bne $t0, $t1, parse_unknown
+
+	# load fourth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fourth character is a newline
+	li $t1, 0xA
+	bne $t0, $t1, parse_unknown
+
+	addi $a0, $zero, 5
+	add $a1, $zero, $zero
 	jr $ra
 
 parse_load:
+	# load second character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if second character is an o
+	li $t1, 0x6F
+	bne $t0, $t1, parse_unknown
+
+	# load third character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if third character is an a
+	li $t1, 0x61
+	bne $t0, $t1, parse_unknown
+
+	# load fourth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fourth character is a d
+	li $t1, 0x64
+	bne $t0, $t1, parse_unknown
+
+	# load fifth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fifth character is a space
+	li $t1, 0x20
+	bne $t0, $t1, parse_unknown
+
+	addi $a0, $zero, 6
+	add $a1, $zero, $zero
 	jr $ra
 
 parse_save:
-	jr $ra
+	# load second character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
 
+	# check if second character is an a
+	li $t1, 0x61
+	bne $t0, $t1, parse_unknown
+
+	# load third character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if third character is an v
+	li $t1, 0x76
+	bne $t0, $t1, parse_unknown
+
+	# load fourth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fourth character is a e
+	li $t1, 0x65
+	bne $t0, $t1, parse_unknown
+
+	# load fifth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fifth character is a space
+	li $t1, 0x20
+	bne $t0, $t1, parse_unknown
+
+	addi $a0, $zero, 7
+	add $a1, $zero, $zero
+	jr $ra
+	
 parse_help:
+	# load second character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if second character is an e
+	li $t1, 0x65
+	bne $t0, $t1, parse_unknown
+
+	# load third character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if third character is an l
+	li $t1, 0x6C
+	bne $t0, $t1, parse_unknown
+
+	# load fourth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fourth character is a p
+	li $t1, 0x70
+	bne $t0, $t1, parse_unknown
+
+	# load fifth character
+	addi $t2, $t2, 1
+	lb $t0, 0($t2)
+
+	# check if fifth character is a newline
+	li $t1, 0xA
+	bne $t0, $t1, parse_unknown
+
+	addi $a0, $zero, 8
+	add $a1, $zero, $zero
 	jr $ra
 
 parse_unknown:
